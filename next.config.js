697e344c187b71b99c37fe42f5c5892795880b53
dhruvparmar2310 <dhanparmar23@gmail.com>
2024-04-07 23:18:43 +0530

    updated members

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,      // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
   images: {
     unoptimized: true
   },
