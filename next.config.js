0c18249bd624aa89aa220d92dd4ee6a5bf99b9f2
dhruvparmar2310 <dhanparmar23@gmail.com>
2024-04-14 19:44:24 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,40 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     unoptimized: true
-//   },
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.pdf$/,
-//       use: {
-//         loader: 'file-loader',
-//         options: {
-//           publicPath: '/_next',
-//           name: 'static/media/[name].[hash].[ext]',
-//         },
-//       },
-//     });
-
-//     return config;
-//   },
-//   env: {
-//     LOCALHOST: 'http://localhost:3000',
-//     DEPLOY: 'https://physiotrends.vercel.app/',
-//   },
-// }
-
-// module.exports = nextConfig
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.(pdf)$/,
+      test: /\.pdf$/,
       use: {
         loader: 'file-loader',
         options: {
@@ -50,10 +22,6 @@ module.exports = withPWA({
     LOCALHOST: 'http://localhost:3000',
     DEPLOY: 'https://physiotrends.vercel.app/',
   },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  },
-});
+}
+
+module.exports = nextConfig
